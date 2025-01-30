e0c138258afc4e5341caa3e607b502769b525847
Durodola Abdulhameed <138580367+olaife219@users.noreply.github.com>
2025-01-30 13:26:38 -0800

    Update page.jsx

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -209,7 +209,7 @@ const Resume = () => {
                             </div>
                         </TabsContent>
                         <TabsContent value="about" className="w-full h-full text-center xl:text-left">
-                            <div className="flex flex-col gap-[30px]">
+                            <div className="flex flex-col gap-[30px] h-full">
                                 <h3 className="text-4xl font-bold">{about.title}</h3>
                                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
                                 <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
