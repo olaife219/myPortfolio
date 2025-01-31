7ae7b2c6be1e85b792373f077b612657a32d9fdf
Durodola Abdulhameed <138580367+olaife219@users.noreply.github.com>
2025-01-31 07:26:06 -0800

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -65,8 +65,8 @@ const Contact = () => {
                             {info.map((item, index)=> {
                                 return (
                                 <li key={index} className="flex items-center gap-6">
-                                    <div className="w-[48px] h-[48px] md:w-[52px] md:h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                                        <div className="text-[23px] md:text-[28px]">{item.icon}</div>
+                                    <div className="w-[45px] h-[45px] md:w-[52px] md:h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                        <div className="text-[20px] md:text-[28px]">{item.icon}</div>
                                     </div>
                                     <div className="flex-1">
                                         <p className="text-white/60">{item.title}</p>
